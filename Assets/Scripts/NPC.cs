7851ffab5fff758d459c76913e1ded4815c25270
Jon-DM-ED <jonm@dm-ed.com>
2023-02-15 19:06:51 +0000

    Add universal AdjustPosition method, combined code dedicated to the left and the right side NPC into one method

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -7,11 +7,15 @@ public class NPC : MonoBehaviour {
     private Transform ball;
     private PlayerController controller;
     private float receivingPosition;
+    private Vector2 forwardVector;
 
     private float ReceivingPosition { get { return receivingPosition; } }
 
     private const float receivingPositionOffset = 0.5f;
+    private const float nearNetZone = 0.5f;
     private const float positioningTolerance = 0.1f;
+    private const float highReboundOffset = 0.25f;
+    private const float lowReboundOffset = 0.25f;
 
     enum Side {
         Left,
@@ -31,6 +35,8 @@ public class NPC : MonoBehaviour {
         ball = BallBehaviour.Instance.transform;
 
         playSide = controller.initPos.x < 0 ? Side.Left : Side.Right;
+        forwardVector = playSide == Side.Left ? Vector2.right : Vector2.left;
+
         SetRecevingPosition(playSide);
     }
 
@@ -51,7 +57,6 @@ public class NPC : MonoBehaviour {
     {
         float displacement = transform.position.x - receivingPosition;
 
-        Debug.Log(ReceivingPosition + " " + displacement);
 
         if (Mathf.Abs(displacement) < positioningTolerance)
         {
@@ -62,23 +67,48 @@ public class NPC : MonoBehaviour {
             if (displacement > 0) controller.MoveLeft();
             if (displacement < 0) controller.MoveRight();
         }
-
-
     }
 
-    private void FollowTheBall()
+    void AdjustPosition()
     {
+        Vector2 toBall = ball.position - transform.position;
 
+        if (Mathf.Abs(ball.position.x) > nearNetZone)
+        {
+            if (toBall.x <= 0.3f && toBall.x >= 0.2f)
+            {
+                controller.Stop();
+                if (ball.position.y < 1.5)
+                    controller.Jump();
+                else
+                    controller.LongJump();
+            }
+            else if (toBall.x > 0.2)
+                controller.MoveRight();
+            else if (toBall.x < 0.3)
+                controller.MoveLeft();
+        }
+        else
+        {
+            if (toBall.x <= 0.2f && toBall.x >= 0.1f)
+            {
+                controller.Stop();
+                if (ball.position.y < 1.5)
+                    controller.Jump();
+                else
+                    controller.LongJump();
+            }
+            else if (toBall.x > 0.1)
+                controller.MoveRight();
+            else if (toBall.x < 0.2)
+                controller.MoveLeft();
+        }
     }
 
     void Update()
     {
-
-
-
         if(controller.initPos.x < 0) // if NPC plays on left field
         {
-            Vector2 toBall = ball.position - transform.position;
 
             if (ball.position.x > 0) // if the ball on enemy's side then move to initPos
             {
@@ -86,41 +116,13 @@ public class NPC : MonoBehaviour {
             }
             else // move towards the ball
             {
-                if (ball.position.x < -0.5)
-                {
-                    if (toBall.x <= 0.3f && toBall.x >= 0.2f)
-                    {
-                        controller.Stop();
-                        if (ball.position.y < 1.5)
-                            controller.Jump();
-                        else
-                            controller.LongJump();
-                    }
-                    else if (toBall.x > 0.2)
-                        controller.MoveRight();
-                    else if (toBall.x < 0.3)
-                        controller.MoveLeft();
-                }
-                else
-                {
-                    if (toBall.x <= 0.2f && toBall.x >= 0.1f)
-                    {
-                        controller.Stop();
-                        if (ball.position.y < 1.5)
-                            controller.Jump();
-                        else
-                            controller.LongJump();
-                    }
-                    else if (toBall.x > 0.1)
-                        controller.MoveRight();
-                    else if (toBall.x < 0.2)
-                        controller.MoveLeft();
-                }
+                AdjustPosition();
             }
         }
         else // if NPC plays on the right side
         {
             Vector2 toBall = transform.position - ball.position;
+
 
             if (ball.position.x < 0) // if the ball on enemy's side then move to initPos
             {
